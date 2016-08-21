642c900d51db62cb95f6fbdf83bc5b5f87bc1c5f
Tim <timothyjings@gmail.com>
2016-08-21 21:57:36 +0800

    Adds focus condition

--- ElvUI_RecountIGF.lua
+++ ElvUI_RecountIGF.lua
@@ -1,18 +1,16 @@
 local frame = CreateFrame("Frame", "EUIRIGF_EVENTFRAME")
 local inCombat = false
 local hasTarget = false
+local hasFocus = false
 local mouseOver = false
 
 local function Update()
-    if inCombat or hasTarget or mouseOver then
-        --Recount.MainWindow:Show()
+    if inCombat or hasTarget or hasFocus or mouseOver then
         Recount_MainWindow:SetAlpha(1.0)
     else
-        --Recount.MainWindow:Hide()
         local globalFadeAlpha = ElvDB["profiles"]["TIM"]["actionbar"]["globalFadeAlpha"]
         Recount_MainWindow:SetAlpha(globalFadeAlpha)
     end
-    print("DEBUG: inCombat, hasTarget, mouseOver = ", inCombat, hasTarget, mouseOver)
 end
 
 -- Handler
@@ -22,13 +20,17 @@ local function OnEventHandler(self, event, ...)
     elseif event == "PLAYER_REGEN_ENABLED" then -- Leave combat
         inCombat = false
     elseif event == "PLAYER_TARGET_CHANGED" then -- Target changed
-        if UnitName("target") == nil then -- Got a new target
-            hasTarget = false
-        else -- Lost our target
+        if UnitExists("target") then -- Got new target
             hasTarget = true
+        else -- Lost target
+            hasTarget = false
         end
-    elseif event == "!@#!@#---" then -- Losing health
-        Recount.MainWindow:Show()
+    elseif event == "PLAYER_FOCUS_CHANGED" then
+        if UnitExists("focus") then -- Got new focus
+            hasFocus = true
+        else -- Lost focus
+            hasFocus = false
+        end
     end
     Update()
 end
@@ -45,6 +47,7 @@ end
 frame:RegisterEvent("PLAYER_REGEN_DISABLED") -- Enter combat
 frame:RegisterEvent("PLAYER_REGEN_ENABLED") -- Leave combat
 frame:RegisterEvent("PLAYER_TARGET_CHANGED") -- Target changed
+frame:RegisterEvent("PLAYER_FOCUS_CHANGED") -- Focus changed
 -- Mouse enter/leave
 Recount_MainWindow:SetScript("OnEnter", Recount_OnEnter)
 Recount_MainWindow:SetScript("OnLeave", Recount_OnLeave)
