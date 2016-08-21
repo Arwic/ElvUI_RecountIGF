ffb8d1d7d4c3f8b159c985a57ac2c4dadae925e6
Tim <timothyjings@gmail.com>
2016-08-22 00:22:16 +0800

    Uses current ElvUI profile

--- ElvUI_RecountIGF.lua
+++ ElvUI_RecountIGF.lua
@@ -1,19 +1,35 @@
-local frame = CreateFrame("Frame", "EUIRIGF_EVENTFRAME")
+local eventFrame = CreateFrame("Frame", "EUIRIGF_EVENTFRAME")
+local hoverFrame = CreateFrame("Frame", "EUIRIGF_HOVERFRAME")
 local inCombat = false
 local hasTarget = false
 local hasFocus = false
 local mouseOver = false
 
+-- Returns the currently active ElvUI profile table
+local function GetCurrentProfile()
+    local charName = GetUnitName("player", false)
+    local realmName = GetRealmName()
+    local charNameRealm = charName .. " - " .. realmName
+    for k, v in pairs(ElvDB["profileKeys"]) do
+        if k == charNameRealm then
+            return ElvDB["profiles"][v]
+        end
+    end
+end
+
+-- Updates the state of recount
 local function Update()
     if inCombat or hasTarget or hasFocus or mouseOver then
         Recount_MainWindow:SetAlpha(1.0)
     else
-        local globalFadeAlpha = ElvDB["profiles"]["TIM"]["actionbar"]["globalFadeAlpha"]
-        Recount_MainWindow:SetAlpha(globalFadeAlpha)
+        local globalFadeAlpha = GetCurrentProfile()["actionbar"]["globalFadeAlpha"]
+        Recount_MainWindow:SetAlpha(1.0 - globalFadeAlpha)
     end
+Recount_MainWindow:SetScript("OnEnter", Recount_OnEnter)
+Recount_MainWindow:SetScript("OnLeave", Recount_OnLeave)
 end
 
--- Handler
+-- Handlers
 local function OnEventHandler(self, event, ...)
     if event == "PLAYER_REGEN_DISABLED" then -- Enter combat
         inCombat = true
@@ -37,21 +53,24 @@ end
 
 local function Recount_OnEnter(self, event, ...)
     mouseOver = true
+    Update()
 end
 
 local function Recount_OnLeave(self, event, ...)
     mouseOver = false
+    Update()
 end
 
 -- Register events
-frame:RegisterEvent("PLAYER_REGEN_DISABLED") -- Enter combat
-frame:RegisterEvent("PLAYER_REGEN_ENABLED") -- Leave combat
-frame:RegisterEvent("PLAYER_TARGET_CHANGED") -- Target changed
-frame:RegisterEvent("PLAYER_FOCUS_CHANGED") -- Focus changed
+eventFrame:RegisterEvent("PLAYER_REGEN_DISABLED") -- Enter combat
+eventFrame:RegisterEvent("PLAYER_REGEN_ENABLED") -- Leave combat
+eventFrame:RegisterEvent("PLAYER_TARGET_CHANGED") -- Target changed
+eventFrame:RegisterEvent("PLAYER_FOCUS_CHANGED") -- Focus changed
 -- Mouse enter/leave
 Recount_MainWindow:SetScript("OnEnter", Recount_OnEnter)
 Recount_MainWindow:SetScript("OnLeave", Recount_OnLeave)
+
 -- Register handlers
-frame:SetScript("OnEvent", OnEventHandler) -- Generic handler
+eventFrame:SetScript("OnEvent", OnEventHandler) -- Generic handler
 Update()
 print("ElvUI_RecountIGF: Loaded")
